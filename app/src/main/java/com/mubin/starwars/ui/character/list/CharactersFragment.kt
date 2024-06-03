36f50fcacfc43d61ce42f3ab711352292d00ff7d
Mubin <maksudmubin@gmail.com>
2024-06-03 16:09:42 +0600

    # CharactersFragment finalized

--- app/src/main/java/com/mubin/starwars/ui/character/list/CharactersFragment.kt
+++ app/src/main/java/com/mubin/starwars/ui/character/list/CharactersFragment.kt
@@ -5,17 +5,33 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.StaggeredGridLayoutManager.VERTICAL
+import com.mubin.starwars.R
 import com.mubin.starwars.base.utils.Constants.CHARACTERS
+import com.mubin.starwars.base.utils.RecyclerViewLazyLoading
+import com.mubin.starwars.base.utils.attachSpeedyLayoutManager
 import com.mubin.starwars.databinding.FragmentCharactersBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class CharactersFragment : Fragment() {
+class CharactersFragment : Fragment(){
 
     private lateinit var binding: FragmentCharactersBinding
+    private lateinit var lazyLoading: RecyclerViewLazyLoading
+    private var characterAdapter: CharacterListAdapter = CharacterListAdapter()
     private val vm: CharacterListViewModel by viewModels()
+
+    private val loadMoreListener = object : RecyclerViewLazyLoading.Listener {
+        override fun loadMore() {
+            if (vm.next.value != null) {
+                vm.getCharacters(vm.next.value ?: CHARACTERS)
+            }
+        }
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,9 +50,22 @@ class CharactersFragment : Fragment() {
 
     }
 
+    override fun onResume() {
+        super.onResume()
+        lazyLoading.registerScrollListener(binding.charactersRv, loadMoreListener)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        lazyLoading.removeListener()
+        viewModelStore.clear()
+    }
+
     private fun initView() {
+        lazyLoading = RecyclerViewLazyLoading.getInstance()
+        binding.charactersRv.attachSpeedyLayoutManager(VERTICAL, false)
         with(binding.charactersRv) {
-            adapter = CharacterListAdapter()
+            adapter = characterAdapter
             isNestedScrollingEnabled = false
             setHasFixedSize(false)
         }
@@ -44,20 +73,31 @@ class CharactersFragment : Fragment() {
 
     private fun setUpObserver() {
         vm.apply {
-            //getCharacters(CHARACTERS)
+
+            getCharacters(CHARACTERS)
 
             isLoading.observe(viewLifecycleOwner) { isLoading ->
 
                 if (isLoading) {
-                    binding.countTv.visibility = View.GONE
-                    binding.charactersRv.visibility = View.GONE
+                    if (next.value.isNullOrEmpty()) {
+                        binding.progressBarLoadingMore.visibility = View.GONE
+                        binding.progressBar.visibility = View.VISIBLE
+                        binding.countTv.visibility = View.GONE
+                        binding.charactersRv.visibility = View.GONE
+                    } else {
+                        binding.progressBarLoadingMore.visibility = View.VISIBLE
+                    }
+
                 } else {
+                    binding.progressBarLoadingMore.visibility = View.GONE
+                    binding.progressBar.visibility = View.GONE
                     binding.countTv.visibility = View.VISIBLE
                     binding.charactersRv.visibility = View.VISIBLE
                 }
 
             }
 
+            com.google.android.material.R.attr.linearProgressIndicatorStyle
             count.observe(viewLifecycleOwner) { count ->
                 val textToSet = if ((count ?: 0) > 1) {
                     "${count.toString()} Characters Found"
@@ -69,7 +109,11 @@ class CharactersFragment : Fragment() {
 
             characterList.observe(viewLifecycleOwner) { list ->
 
-                Log.d("ListCount", "${list?.size}")
+                if (list != null) {
+                    characterAdapter.addDataSet(list) {
+                        Log.d("loadData", "success $it")
+                    }
+                }
 
             }
 
