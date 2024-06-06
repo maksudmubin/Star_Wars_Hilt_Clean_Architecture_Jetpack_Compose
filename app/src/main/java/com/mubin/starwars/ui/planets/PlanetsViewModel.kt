4ced60091485391f9db234ab579f803795668140
Mubin <maksudmubin@gmail.com>
2024-06-06 18:08:25 +0600

    # PlanetsViewModel added

---
+++ app/src/main/java/com/mubin/starwars/ui/planets/PlanetsViewModel.kt
@@ -0,0 +1,26 @@
+package com.mubin.starwars.ui.planets
+
+import androidx.lifecycle.ViewModel
+import com.mubin.starwars.data.model.PlanetListResponse
+import com.mubin.starwars.domain.usecases.planet.GetPlanetsUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+@HiltViewModel
+class PlanetsViewModel @Inject constructor(private val useCase: GetPlanetsUseCase) : ViewModel() {
+
+    val uiState by lazy { PlanetsUIState() }
+    suspend fun getPlanets(url: String) : PlanetListResponse? {
+
+        return withContext(Dispatchers.IO) {
+            useCase.run(url)
+        }
+
+    }
+
+
+
+}
+
