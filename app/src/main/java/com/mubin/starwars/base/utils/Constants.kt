fcd3e9a65083e9deb6a845106cfa60244fe3b8c4
Mubin <maksudmubin@gmail.com>
2024-06-06 18:05:34 +0600

    # new constant added

--- app/src/main/java/com/mubin/starwars/base/utils/Constants.kt
+++ app/src/main/java/com/mubin/starwars/base/utils/Constants.kt
@@ -4,5 +4,6 @@ object Constants {
 
     const val BASE_URL = "https://swapi.dev/api/"
     const val CHARACTERS = "https://swapi.dev/api/people"
+    const val PLANETS = "https://swapi.dev/api/planets"
 
 }
