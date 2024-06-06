7f443828fd8968c883f58a98d73cb8c3eac70dbf
Mubin <maksudmubin@gmail.com>
2024-06-06 18:06:44 +0600

    # compose style files added

---
+++ app/src/main/java/com/mubin/starwars/base/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.mubin.starwars.base.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryBg = Color(0xFFFFFFFF)
+val PrimaryBgDark = Color(0xFF141A1C)
+
+val parentBg = Color(0xFFFEF7FF)
+val parentBgDark = Color(0xFF141218)
+
+val borderStroke = Color(0xFFFFFFFF)
+val borderStrokeDark = Color(0x80CAC4D0)
+
+val textOnPrimaryDark = Color(0xFFFFFFFF)
+val textOnPrimary = Color(0xFF252525)
+val textOnPrimary2 = Color(0xFF111B45)
---
+++ app/src/main/java/com/mubin/starwars/base/theme/Theme.kt
@@ -0,0 +1,75 @@
+package com.mubin.starwars.base.theme
+
+import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+import com.mubin.starwars.base.utils.getActivity
+
+private val DarkColorScheme = darkColorScheme(
+    primary = PrimaryBgDark,
+    onPrimary = textOnPrimaryDark,
+    secondary = textOnPrimaryDark,
+    tertiary = borderStrokeDark,
+    background = parentBgDark
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = PrimaryBg,
+    onPrimary = textOnPrimary,
+    secondary = textOnPrimary2,
+    tertiary = borderStroke,
+    background = parentBg
+
+    /* Other default colors to override
+    background = Color(0xFFFFFBFE),
+    surface = Color(0xFFFFFBFE),
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onTertiary = Color.White,
+    onBackground = Color(0xFF1C1B1F),
+    onSurface = Color(0xFF1C1B1F),
+    */
+)
+
+@Composable
+fun StarWarsTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            if (darkTheme) DarkColorScheme else LightColorScheme
+        }
+
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val activity = view.context.getActivity()
+            if (activity !=null) {
+                val window = activity.window
+                window.statusBarColor = colorScheme.primary.toArgb()
+                WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars =
+                    !darkTheme
+            }
+        }
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
