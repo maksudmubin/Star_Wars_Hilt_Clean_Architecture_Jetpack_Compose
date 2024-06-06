e3592283ae713daf44aba5fb3df5a6f81e7f6dc0
Mubin <maksudmubin@gmail.com>
2024-06-06 18:07:02 +0600

    # GetPlanetsUseCase added

---
+++ app/src/main/java/com/mubin/starwars/domain/usecases/planet/GetPlanetsUseCase.kt
@@ -0,0 +1,14 @@
+package com.mubin.starwars.domain.usecases.planet
+
+import com.mubin.starwars.base.utils.UseCase
+import com.mubin.starwars.data.model.PlanetListResponse
+import com.mubin.starwars.data.repo.AppRepository
+import javax.inject.Inject
+
+class GetPlanetsUseCase @Inject constructor(private val repository: AppRepository) : UseCase<PlanetListResponse, String>() {
+    override suspend fun run(params: String): PlanetListResponse? {
+        return repository.getPlanets(params)
+    }
+
+
+}
