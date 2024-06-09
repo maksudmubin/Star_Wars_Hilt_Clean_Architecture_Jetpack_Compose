d6b3ef1780017fbae8546778e52f693b504e8c9b
Mubin <maksudmubin@gmail.com>
2024-06-09 16:55:09 +0600

    # CharacterListViewModel updated

--- app/src/main/java/com/mubin/starwars/ui/character/list/CharacterListViewModel.kt
+++ app/src/main/java/com/mubin/starwars/ui/character/list/CharacterListViewModel.kt
@@ -1,7 +1,6 @@
 package com.mubin.starwars.ui.character.list
 
 import android.util.Log
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mubin.starwars.data.model.CharacterListResponse
@@ -9,38 +8,18 @@ import com.mubin.starwars.domain.usecases.character.GetCharactersUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
 class CharacterListViewModel @Inject constructor(private val useCase: GetCharactersUseCase) : ViewModel() {
 
-    private val _isLoading = MutableLiveData<Boolean>().apply {
-        value = false
-    }
-    val isLoading = _isLoading
+    val uiState by lazy { CharactersUIState() }
 
-    private val _next = MutableLiveData<String?>()
-    val next = _next
+    suspend fun getCharacters(url: String): CharacterListResponse? {
 
-    private val _count = MutableLiveData<Int?>()
-    val count = _count
-
-    private val _characterList = MutableLiveData<List<CharacterListResponse.Result?>?>()
-    val characterList: MutableLiveData<List<CharacterListResponse.Result?>?> = _characterList
-
-    fun getCharacters(url: String = "") {
-        try {
-            _isLoading.value = true
-            viewModelScope.launch(Dispatchers.Main) {
-                val response = useCase(url)
-                _count.value = response?.count
-                _next.value = response?.next
-                _characterList.value = response?.results
-                _isLoading.value = false
-            }
-        } catch (e:Exception) {
-            Log.d("errorFromAPi", "${e.message}")
-            _isLoading.value = false
+        return withContext(Dispatchers.IO) {
+            useCase.run(url)
         }
 
     }
