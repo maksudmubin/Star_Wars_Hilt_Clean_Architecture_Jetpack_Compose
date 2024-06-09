148aed7cb6db0f0d7e7662f6640aef1e124c988d
Mubin <maksudmubin@gmail.com>
2024-06-09 16:39:13 +0600

    # PlanetsUIState finalized

--- app/src/main/java/com/mubin/starwars/ui/planets/PlanetsUIState.kt
+++ app/src/main/java/com/mubin/starwars/ui/planets/PlanetsUIState.kt
@@ -1,6 +1,7 @@
 package com.mubin.starwars.ui.planets
 
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -9,7 +10,10 @@ import com.mubin.starwars.data.model.PlanetListResponse
 class PlanetsUIState {
 
     var isLoading by mutableStateOf(false)
+    var isMoreLoading by mutableStateOf(false)
     var showRootLayout by mutableStateOf(false)
+    var totalCount by mutableIntStateOf(0)
+    var nextPage by mutableStateOf("")
 
     var response: PlanetListResponse? by mutableStateOf(null)
     var planetList = mutableStateListOf<PlanetListResponse.Result?>()
