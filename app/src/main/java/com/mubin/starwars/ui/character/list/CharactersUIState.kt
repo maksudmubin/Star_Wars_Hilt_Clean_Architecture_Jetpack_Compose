6c2cd2faf11577c620a71b7a05cfd261c181ec68
Mubin <maksudmubin@gmail.com>
2024-06-09 16:54:52 +0600

    # CharactersUIState added

---
+++ app/src/main/java/com/mubin/starwars/ui/character/list/CharactersUIState.kt
@@ -0,0 +1,22 @@
+package com.mubin.starwars.ui.character.list
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import com.mubin.starwars.data.model.CharacterListResponse
+import com.mubin.starwars.data.model.PlanetListResponse
+
+class CharactersUIState {
+
+    var isLoading by mutableStateOf(false)
+    var isMoreLoading by mutableStateOf(false)
+    var showRootLayout by mutableStateOf(false)
+    var totalCount by mutableIntStateOf(0)
+    var nextPage by mutableStateOf("")
+
+    var response: CharacterListResponse? by mutableStateOf(null)
+    var characterList = mutableStateListOf<CharacterListResponse.Result?>()
+
+}
